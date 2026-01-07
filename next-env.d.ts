3ca72b1f43cde7566b5b62de78cd6831f8b587b1
lenz <lenzyounda@gmail.com>
2026-01-07 14:04:02 +0100

    ff

---
+++ next-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+<<<<<<< HEAD
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+=======
+/// <reference path="./.next/types/routes.d.ts" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+>>>>>>> d8a1ab068cb0277dd5c6b1f33c9b850c474457ce
